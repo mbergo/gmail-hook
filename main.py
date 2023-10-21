66282e976f4e4267863d8f2a025191f765f7da69
Marcus Bergo <marcus@bergo.one>
2023-10-21 15:46:52 -0300

    adding next_move

--- main.py
+++ main.py
@@ -75,29 +75,3 @@ def analyse_email(email: Email):
         "suggestion": suggestions,
         "next_move": next_move
     }
-
-
-# email = """
-# Dear Jason 
-# I hope this message finds you well. I'm Shirley from Gucci;
-
-# I'm looking to purchase some company T-shirt for my team, we are a team of 100k people, and we want to get 2 t-shirt per personl
-
-# Please let me know the price and timeline you can work with;
-
-# Looking forward
-
-# Shirley Lou
-# """
-
-# prompt = f"Please extract key information from this email: {email} "
-# message = [{"role": "user", "content": prompt}]
-
-# response = openai.ChatCompletion.create(
-#     model="gpt-4-0613",
-#     messages=message,
-#     functions = function_descriptions,
-#     function_call="auto"
-# )
-
-# print(response)

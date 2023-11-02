52145e4f274b769e1fc6d0f9ae1b983906765ae3
Marcus Bergo <marcus@bergo.one>
2023-11-02 14:02:15 -0300

    commiting structure

--- main.py
+++ main.py
@@ -34,12 +34,16 @@ function_descriptions = [
                     "type": "string",
                     "description": "The suggested actions that the user should take. If there is more than one, please separate them with a comma."
                 },
+                "potential_pathologies": {
+                    "type": "string",
+                    "description": "The potential pathologies that the user might have. If there is more than one, please separate them with a comma."
+                },
                 "summary": {
                     "type": "string",
                     "description": "The summary of the email regarding as a psychological session."
                 }
             },
-            "required": ["psichologicalState", "mainProblem", "suggestedActions", "summary"]
+            "required": ["psichologicalState", "mainProblem", "suggestedActions", "potential_pathologies" "summary"]
         }
     }
 ]
@@ -70,37 +74,13 @@ def analyse_email(email: Email):
     psichologicalState = eval(arguments).get("psichologicalState")
     mainProblem = eval(arguments).get("mainProblem")
     suggestedActions = eval(arguments).get("suggestedActions")
+    potential_pathologies = eval(arguments).get("potential_pathologies")
     summary = eval(arguments).get("summary")
 
     return {
         "psichologicalState": psichologicalState,
         "mainProblem": mainProblem,
         "suggestedActions": suggestedActions,
+        "potential_pathologies": potential_pathologies,
         "summary": summary
         }
-
-
-# email = """
-# Dear Jason 
-# I hope this message finds you well. I'm Shirley from Gucci;
-
-# I'm looking to purchase some company T-shirt for my team, we are a team of 100k people, and we want to get 2 t-shirt per personl
-
-# Please let me know the price and timeline you can work with;
-
-# Looking forward
-
-# Shirley Lou
-# """
-
-# prompt = f"Please extract key information from this email: {email} "
-# message = [{"role": "user", "content": prompt}]
-
-# response = openai.ChatCompletion.create(
-#     model="gpt-4-0613",
-#     messages=message,
-#     functions = function_descriptions,
-#     function_call="auto"
-# )
-
-# print(response)

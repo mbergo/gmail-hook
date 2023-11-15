4602e653daff55c9161f32fdf67bd41990184b74
Marcus Bergo <marcus@bergo.one>
2023-11-15 01:14:04 -0300

    fix

--- main.py
+++ main.py
@@ -1,48 +1,48 @@
-import fastapi
-import openai
-import os
-from dotenv import load_dotenv
-from fastapi import FastAPI
-from pydantic import BaseModel
+    import fastapi
+    import openai
+    import os
+    from dotenv import load_dotenv
+    from fastapi import FastAPI
+    from pydantic import BaseModel
 
 
 
 
 
-load_dotenv()
+    load_dotenv()
 
-app = FastAPI()
+    app = FastAPI()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
-function_descriptions = [
-    {
-        "name": "extract_info_from_email",
-        "description": "Categorize and extract key information from email, such as people, tasks, problems, etc.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "summary": {
-                    "type": "string",
-                    "description": "The summary of the email writen in PT-BR."
-                },                                        
-                "tasks": {
-                    "type": "string",
-                    "description": "The tasks of the email writen in PT-BR. If there is more than one, please separate them with a comma."
+    function_descriptions = [
+        {
+            "name": "extract_info_from_email",
+            "description": "Categorize and extract key information from email, such as people, tasks, problems, etc.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "summary": {
+                        "type": "string",
+                        "description": "The summary of the email writen in PT-BR."
+                    },                                        
+                    "tasks": {
+                        "type": "string",
+                        "description": "The tasks of the email writen in PT-BR. If there is more than one, please separate them with a comma."
+                    },
+                    "problems": {
+                        "type": "string",
+                        "description": "The problems of the email writen in PT-BR. If there is more than one, please separate them with a comma."
+                    },
+                    "conclusion": {
+                        "type": "string",
+                        "description": "The main idea that can be inferred from the email. Please write in PT-BR."
+                    }
                 },
-                "problems": {
-                    "type": "string",
-                    "description": "The problems of the email writen in PT-BR. If there is more than one, please separate them with a comma."
-                },
-                "conclusion": {
-                    "type": "string",
-                    "description": "The main idea that can be inferred from the email. Please write in PT-BR."
-                }
-            },
-            "required": ["summary", "tasks", "problems", "conclusion" ]
+                "required": ["summary", "tasks", "problems", "conclusion" ]
+            }
         }
-    }
-]
+    ]
 
 
 class Email(BaseModel):
@@ -51,7 +51,7 @@ class Email(BaseModel):
 
 def chat_completion_request(model, messages, functions, function_call):
     return openai.ChatCompletion.create(
-        model="gpt-4-1106-preview",
+        model=model,
         messages=messages,
         functions=functions,
         function_call=function_call
@@ -64,29 +64,26 @@ def read_root():
 @app.post("/")
 def analyse_email(email: Email):
     content = email.content
-    query = f"Please extract key information from this email: {content} "
+    query = f"Please extract key information from this email: {content}"
 
     messages = [{"role": "user", "content": query}]
 
     response = chat_completion_request(
-        model="gpt-4-1106-preview",
+        model="gpt-4-1106-preview",  # Verify if this model name is current
         messages=messages,
-        functions = function_descriptions,
+        functions=function_descriptions,
         function_call="auto"
     )
 
-    summary = eval(response.choices[0].json)["summary"]
-    tasks = eval(response.choices[0].).json["tasks"]
-    problems = eval(response.choices[0].json)["problems"]
-    conclusion = eval(response.choices[0].json)["conclusion"]
-
-
-
-    
+    response_data = response.choices[0].json
+    summary = response_data["summary"]
+    tasks = response_data["tasks"]
+    problems = response_data["problems"]
+    conclusion = response_data["conclusion"]
 
     return {
         "summary": summary,
         "tasks": tasks,
         "problems": problems,
         "conclusion": conclusion     
-        }
+    }
